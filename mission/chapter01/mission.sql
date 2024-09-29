a0891284c4240b133c709fb2840b4b953f646abc
jjiinaaa <qka0103@gmail.com>
2024-09-29 23:45:14 +0900

    Mission: Modify week1 mission (after study)

--- mission/chapter01/mission.sql
+++ mission/chapter01/mission.sql
@@ -11,32 +11,13 @@ CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`user` (
-  `id` BIGINT UNSIGNED NOT NULL,
-  `user_id` VARCHAR(255) NOT NULL,
-  `user_password` VARCHAR(255) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `gender` CHAR(1) NOT NULL,
-  `birth_data` DATE NOT NULL,
-  `address` VARCHAR(255) NOT NULL,
-  `prefer_food_id` BIGINT UNSIGNED NOT NULL,
-  `my_point` INT NOT NULL DEFAULT 0,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `mydb`.`area`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`area` (
-  `area_id` BIGINT UNSIGNED NOT NULL,
-  `area_name` VARCHAR(45) NULL,
-  `area_address` VARCHAR(45) NULL,
-  PRIMARY KEY (`area_id`))
+  `id` BIGINT UNSIGNED NOT NULL,
+  `areaName` VARCHAR(45) NULL,
+  `areaAddress` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -44,18 +25,19 @@ ENGINE = InnoDB;
 -- Table `mydb`.`shop`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`shop` (
-  `shop_id` BIGINT UNSIGNED NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL,
   `area_id` BIGINT UNSIGNED NOT NULL,
-  `shop_name` VARCHAR(255) NOT NULL,
+  `shopName` VARCHAR(255) NOT NULL,
+  `rating` DOUBLE NOT NULL DEFAULT 0.0,
   `review_id` BIGINT UNSIGNED NOT NULL,
   `prefer_food_id` BIGINT UNSIGNED NOT NULL,
-  PRIMARY KEY (`shop_id`),
+  PRIMARY KEY (`id`),
   INDEX `area_id_idx1` (`area_id` ASC) VISIBLE,
   UNIQUE INDEX `review_id_UNIQUE` (`review_id` ASC) VISIBLE,
   UNIQUE INDEX `prefer_food_id_UNIQUE` (`prefer_food_id` ASC) VISIBLE,
   CONSTRAINT `area_id`
     FOREIGN KEY (`area_id`)
-    REFERENCES `mydb`.`area` (`area_id`)
+    REFERENCES `mydb`.`area` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -65,23 +47,23 @@ ENGINE = InnoDB;
 -- Table `mydb`.`mission`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`mission` (
-  `mission_id` BIGINT UNSIGNED NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL,
   `area_id` BIGINT UNSIGNED NOT NULL,
   `shop_id` BIGINT UNSIGNED NOT NULL,
   `point` INT NOT NULL DEFAULT 0,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`mission_id`),
+  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
   INDEX `area_id_idx` (`area_id` ASC) VISIBLE,
   INDEX `shop_id_idx` (`shop_id` ASC) VISIBLE,
   CONSTRAINT `area_id`
     FOREIGN KEY (`area_id`)
-    REFERENCES `mydb`.`area` (`area_id`)
+    REFERENCES `mydb`.`area` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `shop_id`
     FOREIGN KEY (`shop_id`)
-    REFERENCES `mydb`.`shop` (`shop_id`)
+    REFERENCES `mydb`.`shop` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -92,19 +74,85 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`user_mission` (
   `id` BIGINT UNSIGNED NOT NULL,
+  `user_id` BIGINT UNSIGNED NOT NULL,
   `mission_id` BIGINT UNSIGNED NOT NULL,
-  `status` TINYINT NULL DEFAULT 0,
-  PRIMARY KEY (`id`, `mission_id`),
+  `area_id` BIGINT UNSIGNED NOT NULL,
+  `area_mission_id` BIGINT UNSIGNED NOT NULL,
+  `status` BIGINT NOT NULL DEFAULT 0,
+  `missionPoint` INT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `status_UNIQUE` (`status` ASC) VISIBLE,
+  UNIQUE INDEX `area_id_UNIQUE` (`area_id` ASC) VISIBLE,
+  INDEX `mission_id_idx` (`mission_id` ASC, `area_id` ASC) VISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `area_id_UNIQUE` (`area_mission_id` ASC) VISIBLE,
   CONSTRAINT `id`
-    FOREIGN KEY (`id`)
+    FOREIGN KEY (`user_id`)
     REFERENCES `mydb`.`user` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `mission_id`
-    FOREIGN KEY (`mission_id`)
-    REFERENCES `mydb`.`mission` (`mission_id`)
+    FOREIGN KEY (`mission_id` , `area_id`)
+    REFERENCES `mydb`.`mission` (`id` , `area_id`)
     ON DELETE RESTRICT
-    ON UPDATE CASCADE)
+    ON UPDATE CASCADE,
+  CONSTRAINT `area_mission_id`
+    FOREIGN KEY (`area_mission_id`)
+    REFERENCES `mydb`.`area_mission` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`area_mission`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`area_mission` (
+  `id` BIGINT UNSIGNED NULL,
+  `user_mission_id` BIGINT UNSIGNED NOT NULL,
+  `area_id` BIGINT UNSIGNED NOT NULL,
+  `point` BIGINT UNSIGNED NOT NULL DEFAULT 0,
+  `user_mission_count` BIGINT UNSIGNED NOT NULL DEFAULT 0,
+  UNIQUE INDEX `area_id_UNIQUE` (`user_mission_id` ASC) VISIBLE,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `point_UNIQUE` (`point` ASC) VISIBLE,
+  UNIQUE INDEX `user_mission_count_UNIQUE` (`user_mission_count` ASC) VISIBLE,
+  UNIQUE INDEX `area_id_UNIQUE` (`area_id` ASC) VISIBLE,
+  CONSTRAINT `area_id`
+    FOREIGN KEY (`area_id`)
+    REFERENCES `mydb`.`user_mission` (`area_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`user` (
+  `id` BIGINT UNSIGNED NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `gender` CHAR(1) NOT NULL,
+  `birthData` DATE NOT NULL,
+  `address` VARCHAR(255) NOT NULL,
+  `prefer_food_id` BIGINT UNSIGNED NOT NULL,
+  `point` BIGINT UNSIGNED NOT NULL DEFAULT 0,
+  `user_mission_count` BIGINT UNSIGNED NOT NULL DEFAULT 0,
+  `area_mission_id` BIGINT UNSIGNED NOT NULL,
+  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `point_UNIQUE` (`point` ASC) VISIBLE,
+  UNIQUE INDEX `user_mission_count_UNIQUE` (`user_mission_count` ASC) VISIBLE,
+  UNIQUE INDEX `area_mission_id_UNIQUE` (`area_mission_id` ASC) VISIBLE,
+  INDEX `count_idx` (`area_mission_id` ASC, `user_mission_count` ASC, `point` ASC) VISIBLE,
+  CONSTRAINT `count`
+    FOREIGN KEY (`area_mission_id` , `user_mission_count` , `point`)
+    REFERENCES `mydb`.`area_mission` (`user_mission_id` , `user_mission_count` , `point`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -112,17 +160,17 @@ ENGINE = InnoDB;
 -- Table `mydb`.`prefer_food`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`prefer_food` (
-  `prefer_food_id` BIGINT UNSIGNED NULL,
-  `food_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`prefer_food_id`),
-  UNIQUE INDEX `food_name_UNIQUE` (`food_name` ASC) VISIBLE,
+  `id` BIGINT UNSIGNED NULL,
+  `foodName` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `food_name_UNIQUE` (`foodName` ASC) VISIBLE,
   CONSTRAINT `prefer_food_id`
-    FOREIGN KEY (`prefer_food_id`)
+    FOREIGN KEY (`id`)
     REFERENCES `mydb`.`user` (`prefer_food_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `prefer_food_id2`
-    FOREIGN KEY (`prefer_food_id`)
+    FOREIGN KEY (`id`)
     REFERENCES `mydb`.`shop` (`prefer_food_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -133,15 +181,17 @@ ENGINE = InnoDB;
 -- Table `mydb`.`review`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`review` (
-  `review_id` BIGINT UNSIGNED NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL,
   `user_id` BIGINT UNSIGNED NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `rating` DOUBLE NOT NULL DEFAULT 0.0,
-  PRIMARY KEY (`review_id`, `user_id`),
+  `user_mission_status` BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`, `user_id`),
   INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
+  UNIQUE INDEX `user_mission_status_UNIQUE` (`user_mission_status` ASC) VISIBLE,
   CONSTRAINT `revie_id`
-    FOREIGN KEY (`review_id`)
+    FOREIGN KEY (`id`)
     REFERENCES `mydb`.`shop` (`review_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
@@ -149,7 +199,20 @@ CREATE TABLE IF NOT EXISTS `mydb`.`review` (
     FOREIGN KEY (`user_id`)
     REFERENCES `mydb`.`user` (`id`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `user_mission_status`
+    FOREIGN KEY (`user_mission_status`)
+    REFERENCES `mydb`.`user_mission` (`status`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`table2`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`table2` (
+)
 ENGINE = InnoDB;
 
 
